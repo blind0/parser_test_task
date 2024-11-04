0e3b9b73478007a66a579d9149c6b242e2419924
love_cube <ahapka1@gmail.com>
2024-11-05 01:22:47 +0500

    crawler

--- quotes_to_scrape/quotes_to_scrape/spiders/quotes.py
+++ quotes_to_scrape/quotes_to_scrape/spiders/quotes.py
@@ -1,12 +1,24 @@
 import scrapy
+from scrapy.exceptions import CloseSpider
 
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
-    start_urls = ["https://quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/page/1/"]
 
-    
+
 
     def parse(self, response):
-        pass
+        for quote in response.xpath("//div[@class='quote']"):
+            yield {
+                "author": quote.xpath("span/small[@class='author']/text()").get(),
+                "text": quote.xpath("span[@class='text']/text()").get(),
+                "tags": quote.xpath("div[@class='tags']/a[@class='tag']/text()").getall()
+            }
+        next_page = response.xpath("//li[@class='next']/a/@href").get()
+        if next_page is None:
+            raise CloseSpider("last page")
+        
+        next_page = response.urljoin(next_page)
+        yield scrapy.Request(next_page, callback=self.parse)
